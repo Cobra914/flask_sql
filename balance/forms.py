7d6d97a342bc0a28f02d3bcc0af17655e92591dc
NICO9 <nmmendez9@hotmail.com>
2024-11-23 12:53:46 +0400

    feat: pinta el campo y el mensaje de error al no ingresar un dato

--- balance/forms.py
+++ balance/forms.py
@@ -13,19 +13,19 @@ from wtforms.validators import data_required, number_range
 class Movimientoform(FlaskForm):
     id = HiddenField()
     fecha = DateField('Fecha', validators=[
-        data_required('Debes indicar la fecha del movimiento')
+        data_required('Debes indicar la fecha del movimiento.')
     ])
     concepto = StringField('Concepto', validators=[
-        data_required('No has especificado un concepto para este movimiento')
+        data_required('No has especificado un concepto para este movimiento.')
     ])
     tipo = RadioField(choices=[('I', 'Ingreso'), ('G', 'Gasto')], validators=[
-        data_required('Necesito saber si es un gasto o un ingreso')
+        data_required('Necesito saber si es un gasto o un ingreso.')
     ])
     cantidad = DecimalField('Cantidad', places=2, validators=[
         data_required(
-            'No puede haber un movimiento sin una cantidad asociada'),
+            'No puede haber un movimiento sin una cantidad asociada.'),
         number_range(
-            min=0.1, message='No se permiten cantidades inferiores a 10 centimos')
+            min=0.1, message='No se permiten cantidades inferiores a 10 centimos.')
     ])
 
     submit = SubmitField('Guardar')
